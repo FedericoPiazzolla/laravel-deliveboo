1e5bf0b050971c1799ad23993cbb6924878c8066
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-19 11:57:34 +0100

    add: types in config

---
+++ config/types.php
@@ -0,0 +1,15 @@
+<?php
+return [
+  "Italiano", //1
+  "Giapponese", //2
+  "Cinese", //3
+  "Gelateria", //4
+  "Pesce", //5
+  "Carne", //6
+  "Pizza", //7
+  "FastFood", //8
+  "Kebab", //9
+  "Colazione", //10
+  "Vegetariano", //11
+  "Indiano" //12
+];
