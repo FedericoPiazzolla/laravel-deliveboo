6bc51c0d92139ec59d9b5f1ccf1c765f841dd081
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-02-19 17:39:17 +0100

    add: Show page for a single dish

---
+++ resources/views/admin/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container mt-5">
+        <h2>{{ $dish->name }}</h2>
+        <div>
+            <p class="mt-4">
+                {{ $dish->description }}
+            </p>
+        </div>
+        <div class="mt-4">
+            Slug: {{ $dish->slug }}
+        </div> 
+        <div class="mt-4">
+            Prezzo: {{ $dish->price }}
+        </div> 
+        <div class="mt-4">
+            Disponibilità: {{ $dish->available }}
+        </div> 
+    </div>
+@endsection
