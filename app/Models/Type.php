66390230811f12a1f4d2278f6896ae657675cf6b
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-19 17:11:03 +0100

    add: create slug in Type Model and create Type Migration

---
+++ app/Models/Type.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function SetNameAttribute($_name)
+    {
+        $this->attributes['name'] = $_name;
+        $this->attributes['slug'] = Str::slug('name');  
+    }
+}
