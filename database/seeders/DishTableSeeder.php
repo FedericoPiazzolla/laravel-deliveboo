138f69bbaf9d8a1129af5c23a3a7dc9dcea7344a
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-19 11:17:49 +0100

    add: DishTableSeeder and add this to DatabaseSeeder

--- database/seeders/DishTableSeeder.php
+++ database/seeders/DishTableSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dish;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class DishTableSeeder extends Seeder
 {
@@ -12,8 +15,15 @@ class DishTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $dish = New Dish();
+            $dish->name = $faker->sentence(2);
+            $dish->description = $faker->text(200);
+            $dish->price = $faker->randomFloat(4, 2, 25);
+            $dish->available = $faker->boolean();
+            $dish->save();
+        }
     }
 }
