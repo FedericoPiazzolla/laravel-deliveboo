37445a0f55bf89681a8c1f050fce10adfad74e99
giuliogallo94 <giuliogallo1994@gmail.com>
2024-02-19 17:23:29 +0100

    add: header dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">{{ __('La tua dashboard') }}</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -14,7 +14,7 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        <h4> Bentornato {{ Auth::user()->name }}</h4>
                     </div>
                 </div>
             </div>
