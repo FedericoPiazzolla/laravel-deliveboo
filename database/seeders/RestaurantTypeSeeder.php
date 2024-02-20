40e668cf212a8c61831473428a65d09c5797199a
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-20 17:02:09 +0100

    refactoring: comments in RestaurantTypeSeeder removed

--- database/seeders/RestaurantTypeSeeder.php
+++ database/seeders/RestaurantTypeSeeder.php
@@ -16,12 +16,9 @@ class RestaurantTypeSeeder extends Seeder
      */
     public function run()
     {
-        // $restaurants = Restaurant::all();
-
         $restaurant_types = config('restaurants_types');
 
         foreach ($restaurant_types as $restaurant_type) {
-            // dd($restaurant_type["type_id"]);
             $restaurant = Restaurant::where('id', $restaurant_type["restaurant_id"])->first();
             $restaurant->types()->attach($restaurant_type["type_id"]);
         }
