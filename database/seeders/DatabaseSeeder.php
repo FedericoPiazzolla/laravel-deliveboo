06a2d36631f480c0b6dcb62ab232d28bbf6b31f0
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-20 08:59:02 +0100

    improve: restaurants seeding with user_id

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersTableSeeder::class,
-            DishesTableSeeder::class,
             RestaurantsTableSeeder::class,
+            DishesTableSeeder::class,
             TypesTableSeeder::class,
         ]);
     }
--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
-use App\Models\Restaurant; 
+use App\Models\Restaurant;
+use App\Models\User;
 
 class RestaurantsTableSeeder extends Seeder
 {
@@ -17,9 +18,17 @@ class RestaurantsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10; $i++) 
-        {
+        // Prendo tutto gli utenti
+        $users = User::all();
+
+        // Per ogni utente appena seeddato:
+        foreach ($users as $user) {
             $restaurant = new Restaurant();
+
+            // Ad ogni ristorante assegno l'id dell'utente in iterazione
+            $restaurant->user_id = $user->id;
+
+            // Continuo col riempimento delle colonne rimanenti
             $restaurant->restaurant_email = $faker->email();
             $restaurant->restaurant_name = $faker->sentence(2);
             $restaurant->slug = Str::slug($restaurant->company_name);
