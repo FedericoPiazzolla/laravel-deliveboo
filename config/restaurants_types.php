0d2164803c49fb111212c51882e5e445490bbe5b
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-20 12:50:32 +0100

    add: config restaurants_types

---
+++ config/restaurants_types.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+  [
+    "restaurant_id" => 1,
+    "typology_id" => [1, 11,]
+  ],
+  [
+    "restaurant_id" => 2,
+    "typology_id" => [1, 7, 6]
+  ],
+  [
+    "restaurant_id" => 3,
+    "typology_id" => [1, 6,]
+  ],
+  [
+    "restaurant_id" => 4,
+    "typology_id" => [9, 7]
+  ],
+  [
+    "restaurant_id" => 5,
+    "typology_id" => [2, 3, 5]
+  ],
+  [
+    "restaurant_id" => 6,
+    "typology_id" => [1, 5, 6, 7]
+  ],
+  [
+    "restaurant_id" => 7,
+    "typology_id" => [12, 6, 5]
+  ],
+  [
+    "restaurant_id" => 8,
+    "typology_id" => [4, 10]
+  ],
+  [
+    "restaurant_id" => 9,
+    "typology_id" => [5, 7]
+  ],
+  [
+    "restaurant_id" => 10,
+    "typology_id" => [8, 9, 7]
+  ],
+  [
+    "restaurant_id" => 11,
+    "typology_id" => [3, 2]
+  ],
+  [
+    "restaurant_id" => 12,
+    "typology_id" => [10]
+  ],
+];
