c86ea6fba6552d52ef62205d0cc4f27599fb9476
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-21 16:14:12 +0100

    fix: show and index blade

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,15 +14,31 @@
                             </div>
                         @endif
 
-                        <h4> Bentornato {{ Auth::user()->name }}! ecco il tuo ristorante:</h4>
-                        <h1 class="text-center" style="color: #173736">{{ $restaurant->restaurant_name }}</h1>
+                        <h4 class="mb-4"> Bentornato {{ Auth::user()->name }}! ecco il tuo ristorante:</h4>
+                        <h1 class="text-center mb-4" style="color: #173736">{{ $restaurant->restaurant_name }}</h1>
                         <img src="{{ $restaurant->restaurant_image }}" class="card-img-top mt-3 w-70" alt="...">
                         <div class="card-body">
-                            <p class="card-text">{{ $restaurant->restaurant_email }}</p>
-                            <div>{{ $restaurant->restaurant_address }}</div>
-                            @foreach ($restaurant->types as $type)
-                                <span class="badge text-bg-success"> {{ $type->name }} </span>
-                            @endforeach
+                            <div class="mb-4">
+                                <p class="card-text">
+                                    <span class="fw-bolder">La tua mail:</span>
+                                    {{ $restaurant->restaurant_email }}
+                                </p>
+                            </div>
+                            
+                            <div class="mb-4">
+                                <p>
+                                    <span class="fw-bolder">La tua via:</span>
+                                     {{ $restaurant->restaurant_address }}
+                                </p>
+                            </div>
+
+                            <div class="mb-4">
+                                <p class="fw-bolder">Le tue Tipologie:</p>
+                                @foreach ($restaurant->types as $type)
+                                    <span class="badge text-bg-success"> {{ $type->name }} </span>
+                                @endforeach
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -2,20 +2,37 @@
 
 @section('content')
     <div class="container mt-5">
-        <h2>{{ $dish->name }}</h2>
-        <div>
-            <p class="mt-4">
-                {{ $dish->description }}
-            </p>
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <div class="card p-4">
+                    <h2>{{ $dish->name }}</h2>
+
+                    <div>
+                        <img src="{{ $dish->image }}" alt="{{ $dish->name }}">
+                    </div>
+                    <div>
+                        <p class="mt-4">
+                           <span class="fw-bolder">Descrizione:</span>
+                            {{ $dish->description }}
+                        </p>
+                    </div>
+                    <div class="mt-4">
+                        <p>
+                          <span class="fw-bolder">Prezzo:</span> 
+                            {{ $dish->price }}  € 
+                        </p>
+                        
+                    </div> 
+                    <div class="mt-4">
+                        <p>
+                            <span class="fw-bolder">Disponibilià:</span> 
+                            {{ $dish->available ? 'Disponibile' : 'Non disponibile' }}
+                        </p>
+                        
+                    </div> 
+                </div>
+            </div>
         </div>
-        <div class="mt-4">
-            Slug: {{ $dish->slug }}
-        </div> 
-        <div class="mt-4">
-            Prezzo: {{ $dish->price }} 
-        </div> 
-        <div class="mt-4">
-            Disponibilità: {{ $dish->available ? 'Disponibile' : 'Non disponibile' }}
-        </div> 
+        
     </div>
 @endsection
