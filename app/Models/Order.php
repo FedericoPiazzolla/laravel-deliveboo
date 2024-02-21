150773fa30bff3277d0b5a24ad110f127a0aa0e4
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-02-21 18:12:48 +0100

    add: fillable

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['interested_user_name', 'interested_user_surname', 'interested_user_address', 'interested_user_email', 'interested_user_phone'];
+
     public function dishes(){
         return $this->belongsToMany(Dish::class);
     }
