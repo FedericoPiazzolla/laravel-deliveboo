01f2476f71b0984e2958680e117a9b7377a4007d
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-22 10:52:08 +0100

    add: dish_order config

--- config/dish_order.php
+++ config/dish_order.php
@@ -1,28 +1,51 @@
 <?php
 return [
     [
-        
+        "restaurant_id" => 1,
+        "order_id" => [3, 5],
     ],
     [
-        
+        "restaurant_id" => 2,
+        "order_id" => [1, 6],
     ],
     [
-        
+        "restaurant_id" => 3,
+        "order_id" => [7, 10],
     ],
     [
-        
+        "restaurant_id" => 4,
+        "order_id" => [2, 11],
     ],
     [
-        
+        "restaurant_id" => 5,
+        "order_id" => [13, 17],
     ],
     [
-        
+        "restaurant_id" => 6,
+        "order_id" => [19, 24],
     ],
     [
-        
+        "restaurant_id" => 7,
+        "order_id" => [12, 8],
     ],
     [
-        
+        "restaurant_id" => 8,
+        "order_id" => [9, 4],
     ],
-    
+    [
+        "restaurant_id" => 9,
+        "order_id" => [14, 21],
+    ],
+    [
+        "restaurant_id" => 10,
+        "order_id" => [20, 23],
+    ],
+    [
+        "restaurant_id" => 11,
+        "order_id" => [15, 22],
+    ],
+    [
+        "restaurant_id" => 12,
+        "order_id" => [16, 18],
+    ],
 ];
