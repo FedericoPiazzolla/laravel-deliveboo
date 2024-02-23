76b410b27b792345c2c54f1db078aad660c24de3
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-23 16:46:25 +0100

    fix: orders query

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -14,22 +14,13 @@ class OrderController extends Controller
 {
     public function index()
     {
-        $user = User::where('id', Auth::user()->id)->first();
-
-        $orders = DB::table('orders')
-        ->join('dish_order', 'dish_order.order_id', '=', 'orders.id')
-        ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
-        ->join('restaurants', 'dishes.restaurant_id', '=', 'restaurants.id')
-        ->join('users', 'restaurants.user_id', '=', 'users.id')
-        ->where('users.id', $user->id)
-        ->select('orders.*', 'dishes.*', 'dish_order.*', 'restaurants.*', 'users.*') // Seleziona le colonne che desideri
-        ->get();
+        $orders = Order::with('dishes')
+        ->whereHas('dishes', function($query){
+            $user = User::where('id', Auth::user()->id)->first();
+            $restaurant = Restaurant::where('user_id', $user->id)->first();
+                $query->where('restaurant_id', $restaurant->id);
+            })->get();
 
         return view('admin.order', compact('orders'));
-
-        // $orders = Order::all();
-     
-        return view('admin.order', compact('orders'));
-        
     }
 }
