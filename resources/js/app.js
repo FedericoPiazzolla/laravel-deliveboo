4acfe16f18ddb380b16543ae03926c64c12333d0
giuliogallo94 <giuliogallo1994@gmail.com>
2024-02-23 18:04:37 +0100

    add: import AppModal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import "./bootstrap";
+import "./appModal";
 
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Usando Vite -->
-    @vite(['resources/js/app.js', 'resources/js/validations.js', 'resources/js/appModal.js' ])
+    @vite(['resources/js/app.js', 'resources/js/validations.js' ])
 </head>
 
 <body>
