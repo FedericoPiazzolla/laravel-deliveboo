003e887685e40e8c19ae836fb42ada7f3981c1df
FedericoPiazzolla <Federicop257@gmail.com>
2024-02-24 18:15:26 +0100

    fix: Restaurant controller for types

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -10,30 +10,32 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
  {
-     public function index(Request $request){
-         
-         $restaurantsApi = Restaurant::with('types');
-
-          if ($request->has('type_id')) {
-              $types_id = [$request->type_id];
-              foreach($types_id as $type_id) {
-                  $restaurantsApi->whereHas('types', fn($query) => $query->where('id', $type_id));
-              }
-          }
-
-          $restaurants = $restaurantsApi->paginate(6);
-
-          if($restaurants) {
-            return response()->json([
-                'results' => $restaurants,
-                'success' => true
-            ]);
-        } else {
+    public function index(Request $request)
+    {
+        $restaurantsApi = Restaurant::with('types');
+    
+        if ($request->has('type_id')) {
+            $types_id = explode(',', $request->type_id);
+    
+            foreach ($types_id as $type_id) {
+                $restaurantsApi->whereHas('types', fn($query) => $query->where('id', $type_id));
+            }
+            
+        }
+    
+        $restaurants = $restaurantsApi->get();
+    
+        if ($restaurants->isEmpty()) {
             return response()->json([
                 'success' => false,
-                'message' => 'nessun ristorante trovato'
+                'message' => 'Nessun ristorante trovato'
             ]);
         }
-     }
+    
+        return response()->json([
+            'results' => $restaurants,
+            'success' => true
+        ]);
+    }
    
  }
