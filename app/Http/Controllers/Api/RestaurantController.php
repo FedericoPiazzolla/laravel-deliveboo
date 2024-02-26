6d3e246917dbe90471cff107b74056ff41d1ddfb
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-26 18:08:59 +0100

    add: single restaurant response in restaurants api

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -9,33 +9,40 @@ use App\Models\Type;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
- {
+{
     public function index(Request $request)
     {
         $restaurantsApi = Restaurant::with('types');
-    
+
         if ($request->has('type_id')) {
             $types_id = explode(',', $request->type_id);
-    
+
             foreach ($types_id as $type_id) {
-                $restaurantsApi->whereHas('types', fn($query) => $query->where('id', $type_id));
+                $restaurantsApi->whereHas('types', fn ($query) => $query->where('id', $type_id));
             }
-            
-        }
-    
-        $restaurants = $restaurantsApi->get();
-    
-        if ($restaurants->isEmpty()) {
+
+            $restaurants = $restaurantsApi->get();
+            if ($restaurants->isEmpty()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Nessun ristorante trovato'
+                ]);
+            }
             return response()->json([
-                'success' => false,
-                'message' => 'Nessun ristorante trovato'
+                'results' => $restaurants,
+                'success' => true
             ]);
         }
-    
-        return response()->json([
-            'results' => $restaurants,
-            'success' => true
-        ]);
+
+
+        // Singolo ristorante
+
+        if ($request->has('restaurant_id')) {
+            $restaurant = Restaurant::where('id', $request->restaurant_id)->first();
+            return response()->json([
+                'results' => $restaurant,
+                'success' => true
+            ]);
+        }
     }
-   
- }
+}
