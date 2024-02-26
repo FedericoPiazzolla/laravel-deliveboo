6c19a9bb0f4db1da371bc53981a07f14dec6bbd9
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-26 18:25:22 +0100

    fix: all restaurants response not working, now fixed

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -12,15 +12,37 @@ class RestaurantController extends Controller
 {
     public function index(Request $request)
     {
-        $restaurantsApi = Restaurant::with('types');
+        // Codice per ottenere tutti i ristoranti
+        
+        if(!$request->has('type_id') && !$request->has('restaurant_id')) {
+            $restaurants = Restaurant::all();
+            return response()->json([
+                'results' => $restaurants,
+                'success' => true
+            ]);
+        }
+        
+        // Singolo ristorante
 
+        if ($request->has('restaurant_id')) {
+            $restaurant = Restaurant::where('id', $request->restaurant_id)->first();
+            return response()->json([
+                'results' => $restaurant,
+                'success' => true
+            ]);
+        }
+
+        // Codice per risposta in base agli id delle tipologie
+
+        $restaurantsApi = Restaurant::with('types');
         if ($request->has('type_id')) {
             $types_id = explode(',', $request->type_id);
 
             foreach ($types_id as $type_id) {
                 $restaurantsApi->whereHas('types', fn ($query) => $query->where('id', $type_id));
             }
-
+            
+            // Se non è stato trovato nessun ristorante
             $restaurants = $restaurantsApi->get();
             if ($restaurants->isEmpty()) {
                 return response()->json([
@@ -28,19 +50,10 @@ class RestaurantController extends Controller
                     'message' => 'Nessun ristorante trovato'
                 ]);
             }
+
+            // Altrimenti
             return response()->json([
                 'results' => $restaurants,
-                'success' => true
-            ]);
-        }
-
-
-        // Singolo ristorante
-
-        if ($request->has('restaurant_id')) {
-            $restaurant = Restaurant::where('id', $request->restaurant_id)->first();
-            return response()->json([
-                'results' => $restaurant,
                 'success' => true
             ]);
         }
