ffe414f8cb72b0a83a694ce10295634bfe08b9d0
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-28 10:45:27 +0100

    fix: order post api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -9,7 +9,8 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         // Prendo i dati dalla richiesta
         $form_data = $request->all();
 
@@ -18,11 +19,25 @@ class OrderController extends Controller
 
         // Riempio i campi dell'ordine
         $order->fill($form_data);
-        
+
         // Salvo il nuovo record dell'ordine nella tabella orders
         $order->save();
 
-        // Associo gli id dei piatti nella richiesta all'id dell'ordine appena istanziato
-        $order->dishes()->attach($order->id, [$form_data["dishes_id"]]);
+        // Preparo l'array degli id dei piatti
+        $dishes_id = explode(',', $form_data["dishes_id"]);
+        
+        // Preparo l'array delle quantità di ogni piatto
+        $dishes_quantities = explode(',', $form_data["dishes_quantities"]);
+
+        // Creo un array vuoto che conterrà l'id del piatto con la sua quantità
+        $dishesWithQuantities = [];
+
+        // Ciclo entrambi gli array (id dei piatti e quantità) e li pusho nell'array appena inizializzato
+        for ($i = 0; $i < count($dishes_id); $i++) {
+            $dishesWithQuantities[$dishes_id[$i]] = ['dish_quantity' => $dishes_quantities[$i]];
+        }
+
+        // Associo all'ordine gli id dei piatti con le corrispettive quantità
+        $order->dishes()->attach($dishesWithQuantities);
     }
 }
