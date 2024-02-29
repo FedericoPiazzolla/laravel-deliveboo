2777252607076386c7db4f12f98beabda2944e1c
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-02-29 11:28:57 +0100

    update: comments removed

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -35,27 +35,29 @@ class OrderController extends Controller
             // Salvo il nuovo record dell'ordine nella tabella orders
             $order->save();
 
+            
+            // Preparo l'array degli id dei piatti
+            $dishes_id = explode(',', $form_data["dishes_id"]);
+            
+            // Preparo l'array delle quantità di ogni piatto
+            $dishes_quantities = explode(',', $form_data["dishes_quantities"]);
+            
+            // Creo un array vuoto che conterrà l'id del piatto con la sua quantità
+            $dishesWithQuantities = [];
+            
+            // Ciclo entrambi gli array (id dei piatti e quantità) e li pusho nell'array appena inizializzato
+            for ($i = 0; $i < count($dishes_id); $i++) {
+                $dishesWithQuantities[$dishes_id[$i]] = ['dish_quantity' => $dishes_quantities[$i]];
+            }
+            
+            // Associo all'ordine gli id dei piatti con le corrispettive quantità
+            $order->dishes()->attach($dishesWithQuantities);
+
             return response()->json([
                 "result" => true,
-                "message" => $request->all(),
+                "message" => "Ordine salvato correttamente",
+                "form_data" => $request->all(),
             ]);
-
-            // // Preparo l'array degli id dei piatti
-            // $dishes_id = explode(',', $form_data["dishes_id"]);
-
-            // // Preparo l'array delle quantità di ogni piatto
-            // $dishes_quantities = explode(',', $form_data["dishes_quantities"]);
-
-            // // Creo un array vuoto che conterrà l'id del piatto con la sua quantità
-            // $dishesWithQuantities = [];
-
-            // // Ciclo entrambi gli array (id dei piatti e quantità) e li pusho nell'array appena inizializzato
-            // for ($i = 0; $i < count($dishes_id); $i++) {
-            //     $dishesWithQuantities[$dishes_id[$i]] = ['dish_quantity' => $dishes_quantities[$i]];
-            // }
-
-            // // Associo all'ordine gli id dei piatti con le corrispettive quantità
-            // $order->dishes()->attach($dishesWithQuantities);
         }
     }
 }
