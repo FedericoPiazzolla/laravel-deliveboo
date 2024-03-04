a088a4d4305d6dc9edc3fea1406c592891b4107a
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-03-04 16:25:07 +0100

    favicon added, app name changed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,6 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fontawesome 6 cdn -->
