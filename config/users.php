101ae90a65e7e35fe39bfcca2781f7ad285764d9
giuliogallo94 <giuliogallo1994@gmail.com>
2024-03-04 15:03:43 +0100

    add: new users, restaurants and dishes

--- config/users.php
+++ config/users.php
@@ -96,4 +96,196 @@ return [
         "email" => 'fiorello.occhiello@icloud.com',
         "vat_number" => '',
     ],
+    [
+        "name" => 'Anna',
+        "surname" => 'Verdi',
+        "password" => 'abcd1234',
+        "email" => 'anna.verdi@gmail.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Luca',
+        "surname" => 'Neri',
+        "password" => 'abcd1234',
+        "email" => 'luca.neri@libero.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Giovanni',
+        "surname" => 'Marroni',
+        "password" => 'abcd1234',
+        "email" => 'giovanni.marroni@hotmail.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Elisa',
+        "surname" => 'Bianco',
+        "password" => 'abcd1234',
+        "email" => 'elisa.bianco@outlook.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Simone',
+        "surname" => 'Gialli',
+        "password" => 'abcd1234',
+        "email" => 'simone.gialli@yahoo.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Laura',
+        "surname" => 'Rosa',
+        "password" => 'abcd1234',
+        "email" => 'laura.rosa@gmail.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Marco',
+        "surname" => 'Blu',
+        "password" => 'abcd1234',
+        "email" => 'marco.blu@hotmail.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Sara',
+        "surname" => 'Viola',
+        "password" => 'abcd1234',
+        "email" => 'sara.viola@icloud.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Davide',
+        "surname" => 'Arancione',
+        "password" => 'abcd1234',
+        "email" => 'davide.arancione@gmail.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Martina',
+        "surname" => 'Verde',
+        "password" => 'abcd1234',
+        "email" => 'martina.verde@outlook.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Alessandro',
+        "surname" => 'Giallo',
+        "password" => 'abcd1234',
+        "email" => 'alessandro.giallo@libero.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Giulia',
+        "surname" => 'Arancio',
+        "password" => 'abcd1234',
+        "email" => 'giulia.arancio@yahoo.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Filippo',
+        "surname" => 'Rossa',
+        "password" => 'abcd1234',
+        "email" => 'filippo.rossa@gmail.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Valentina',
+        "surname" => 'Blu',
+        "password" => 'abcd1234',
+        "email" => 'valentina.blu@hotmail.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Riccardo',
+        "surname" => 'Viola',
+        "password" => 'abcd1234',
+        "email" => 'riccardo.viola@outlook.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Chiara',
+        "surname" => 'Gialli',
+        "password" => 'abcd1234',
+        "email" => 'chiara.gialli@yahoo.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Andrea',
+        "surname" => 'Verdi',
+        "password" => 'abcd1234',
+        "email" => 'andrea.verdi@gmail.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Federica',
+        "surname" => 'Neri',
+        "password" => 'abcd1234',
+        "email" => 'federica.neri@libero.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Paolo',
+        "surname" => 'Marroni',
+        "password" => 'abcd1234',
+        "email" => 'paolo.marroni@hotmail.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Eleonora',
+        "surname" => 'Bianco',
+        "password" => 'abcd1234',
+        "email" => 'eleonora.bianco@outlook.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Fabio',
+        "surname" => 'Giallo',
+        "password" => 'abcd1234',
+        "email" => 'fabio.giallo@yahoo.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Silvia',
+        "surname" => 'Rosa',
+        "password" => 'abcd1234',
+        "email" => 'silvia.rosa@gmail.com',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Roberto',
+        "surname" => 'Blu',
+        "password" => 'abcd1234',
+        "email" => 'roberto.blu@hotmail.it',
+        "vat_number" => '',
+    ],
+
+    [
+        "name" => 'Elena',
+        "surname" => 'Viola',
+        "password" => 'abcd1234',
+        "email" => 'elena.viola@icloud.com',
+        "vat_number" => '',
+    ]
+
 ];
